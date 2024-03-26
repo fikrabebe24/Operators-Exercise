0c72de788e204d07faa20a107774a4e4a37a00ad
Fikr Abebe <fikrabebe24@gmail.com>
2024-03-26 14:58:37 -0400

    Operators Exercise Part 1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,6 +4,38 @@
     {
         static void Main(string[] args)
         {
+
+            //Exercise 1, #1---------------------
+            int a = 17;
+            int b = 4;
+
+            var sum = a + b;
+            var diff = a - b;
+            var prod = a * b;
+            double quot = a / b;
+            var mod = a % b;
+
+            //Exercise 1, #2----------------------
+
+            int x = 24;
+            int y = 5;
+
+
+                double quote = x / y;
+            int remainder = x % y;
+
+            //Exercise 1, #3------------------------
+
+            Console.WriteLine($"The sum of {a} + {b} is {sum}.");
+            Console.WriteLine($"When you subtract {b} from {a}, you get {diff}.");
+            Console.WriteLine($"When you multiply {a} by {b}, you get {diff}");
+            Console.WriteLine($"When you divide {a} by {b}, you get {quote}.");
+            Console.WriteLine($"{a} divided by {b} has a remainder of {mod}.");
+            Console.WriteLine($"{x} divided by {y} is {quote}.");
+            Console.WriteLine($"The remainder of {x} divided by {y} is {remainder}.");
+
+
+
             Console.WriteLine("Hello, World!");
         }
     }
